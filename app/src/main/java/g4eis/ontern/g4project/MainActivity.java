9e7fad01d35ac076c73a0e30ebab2fd69d4e765c
Prince <princekumar2996@gmail.com>
2017-07-22 20:10:59 +0530

    AlertBoxes added

--- app/src/main/java/g4eis/ontern/g4project/MainActivity.java
+++ app/src/main/java/g4eis/ontern/g4project/MainActivity.java
@@ -1,11 +1,15 @@
 package g4eis.ontern.g4project;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -22,8 +26,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     EditText etUId, etPwd;
-    Button btnPwd, btnLogin;
-    private PreferenceManager prefManager;
+    Button btnNewUsr, btnLogin, btnForgot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,48 +34,98 @@ public class MainActivity extends AppCompatActivity {
         etUId = (EditText) findViewById(R.id.etid);
         etPwd = (EditText) findViewById(R.id.etPwd);
         btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnPwd = (Button) findViewById(R.id.btnPwd);
+        btnNewUsr = (Button) findViewById(R.id.btnNewUsr);
+        btnForgot = (Button) findViewById(R.id.btnForgot);
 
-        prefManager = new PreferenceManager(this);
-        if (!prefManager.isFirstTimeLaunch()) {
 
-            btnLogin.setEnabled(false);
-        }
-
-        btnPwd.setOnClickListener(new View.OnClickListener() {
+        btnNewUsr.setOnClickListener(new View.OnClickListener() {           //Button for New User Registration
             @Override
-            public void onClick(View view) {
-                String uid=etUId.getText().toString();
-                if(!uid.equals("")) {
-                    getPwd(uid);
-                    if (!prefManager.isFirstTimeLaunch()) {
-                        etUId.setEnabled(false);
-                        btnLogin.setEnabled(true);
-                    }
-                }
-                else{
-                    Toast.makeText(MainActivity.this,"Empty e-mail field, Please fill to continue...",Toast.LENGTH_LONG).show();
-                }
+            public void onClick(final View view) {
+                //Dialog Box for registration
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.InvitationDialog);
+                final EditText input = new EditText(MainActivity.this);      //Text Box for email input
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                builder.setTitle("New User Registration");
+                builder.setCancelable(false);
+                builder.setMessage("Enter Your e-mail to recieve password");
+                builder.setPositiveButton("Send Password", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                if(!input.getText().toString().equals("")) {        //Checking for empty Input
+                                    getPwd(input.getText().toString());  //method call for making password request
+                                }
+                                else{
+                                    Snackbar snackbar = Snackbar.make(view, "Empty e-mail field, Please fill and try again...",
+                                            Snackbar.LENGTH_LONG);
+                                    snackbar.show();
+                                }
+                            }
+                        }
+                );
+                builder.setNegativeButton("Cancel", null);
+                builder.show();
+
+            }
+        });
+
+
+        btnForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.InvitationDialog);
+                final EditText input = new EditText(MainActivity.this);      //Text Box for email input
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                lp.setMargins(5,5,5,5);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                builder.setTitle("Recover Your Password");
+                builder.setCancelable(false);
+                builder.setMessage("Enter your registered e-mail id");
+                builder.setPositiveButton("Send Password", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                if(!input.getText().toString().equals("")) {        //Checking for empty Input
+                                    getPwd(input.getText().toString());  //method call for making password request
+                                }
+                                else{
+                                    Snackbar snackbar = Snackbar.make(view, "Empty e-mail field, Please fill and try again...",
+                                            Snackbar.LENGTH_LONG);
+                                    snackbar.show();
+                                }
+                            }
+                        }
+                );
+                builder.setNegativeButton("Cancel", null);
+                builder.show();
             }
         });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) {        //Button for Login
                 String uid=etUId.getText().toString();
                 String pwd=etPwd.getText().toString();
                 if(!pwd.equals("")){
                     loginUser(uid,pwd);
                 }
                 else{
-                    Toast.makeText(MainActivity.this,"Empty Password field, Please fill to continue...",Toast.LENGTH_LONG).show();
+                    Snackbar snackbar = Snackbar
+                            .make(view, "Empty Password field, Please fill to continue...", Snackbar.LENGTH_LONG);
+
+                    snackbar.show();
+
+                    //Toast.makeText(MainActivity.this,"Empty Password field, Please fill to continue...",Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
 
-    private void loginUser( final String email, final String password){
+    private void loginUser( final String email, final String password){         //method for login request to server and checking response
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);  // this = context
         String url = "http://api.mrasif.in/demo/gchat/login.php?";
@@ -122,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void getPwd( final String email){
+    private void getPwd( final String email){       //method for password request to server
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);  // this = context
         String url = "http://api.mrasif.in/demo/gchat/login.php?";
