7e7712c469115f90116294c6e25e47742e65b13f
Prince <princekumar2996@gmail.com>
2017-07-30 17:06:49 +0530

    Logout added

--- app/src/main/java/g4eis/ontern/g4project/Main2Activity.java
+++ app/src/main/java/g4eis/ontern/g4project/Main2Activity.java
@@ -1,5 +1,8 @@
 package g4eis.ontern.g4project;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,6 +21,8 @@ import g4eis.ontern.g4project.R;
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "MyPrefs";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +76,15 @@ public class Main2Activity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+        if(id==R.id.action_logout){
+            sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.clear();
+            editor.commit();
+            Intent signInIntent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(signInIntent);
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
